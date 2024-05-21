e7cbda80b2bec6eb1f6eac6ca29ff9d716fa3aac
Claudia Simone de Souza <claudia.simmone@gmail.com>
2024-05-21 21:53:40 +0100

    Add project files.

---
+++ RSGymPT/User.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSGymPT
+{
+    internal class User
+    {
+        #region Fields (properties, private variables)
+        /*
+        variáveis internas da classe para serem usadas dentro das propriedades (Classic properties / Bodied-expression properties)
+        */
+
+        #endregion
+
+        #region Properties (public or internal)
+        #region Auto-implemented properties 2.0
+        /* 
+        Exemplo de uma propriedade usando Auto-implemented properties
+        internal string Operator { get; set; } // propriedade no singular
+        */
+
+        internal int Id { get; }
+        internal static int NextId { get; set; } = 1;
+        internal string Name { get; set; }
+
+        #endregion
+
+        #region Classic properties 1.0
+        /* 
+        Exemplo de uma propriedade usando Classic properties
+
+        internal double Value01
+        {
+            get { return value01; }     // Ler o valor da propriedade
+            set { value01 = value; }    // escrever o valor da propriedade
+        }
+        */
+
+        #endregion
+
+        #region Bodied-expression properties 3.0
+        /* 
+        Exemplo de uma propriedade usando Bodied-expression properties
+        internal double Value02
+        {
+            get => value02;         // => lambda operator
+            set => value02 = value;
+        }
+        */
+
+        #endregion
+        #endregion
+
+        #region Constructors (Public)
+        // Fazer substituto do default constructor
+
+
+        // Fazer segundo construtor com inserção parâmetros obrigatórios
+
+        #endregion
+
+        #region Methods (public or internal)
+
+        #endregion
+
+        #region Destructor
+
+        #endregion
+    }
+}
---
+++ Utility/Utility.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class RSGymUtility
+    {
+        // Create a method to ensure that characters from any language, including special characters, can be correctly displayed in the console.
+        // This is particularly useful when working with text that contains characters from languages other than English, as UTF-8 supports a wide range of characters and symbols.
+
+        public static void SetUnicodeConsole()
+        {
+            // Console.WriteLine("á Á à À ã Ã â Â ç Ç º ª");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+        }
+
+        // Create a method to show a stylish title
+        public static void WriteTitle(string title, string beginTitle = "", string endTitle = "")
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"{beginTitle}{new string('-', title.Length)}\n{title.ToUpper()}\n{new string('-', title.Length)}{endTitle}");
+            Console.ForegroundColor = ConsoleColor.White;   // Reset original color
+        }
+
+        // Create a method to show a message setted with skip lines at the beginning and end of it
+        public static void WriteMessage(string message, string beginMessage = "", string endMessage = "")
+        {
+            Console.Write($"{beginMessage}{message}{endMessage}");
+        }
+
+        // Create a method to terminate the console with a stylish message
+        public static void TerminateConsole()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("\n\nPrime qualquer tecla para terminares.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public static void PauseConsole()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("\n\nPrime qualquer tecla para continuar.\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+
+        
+    }
+}
