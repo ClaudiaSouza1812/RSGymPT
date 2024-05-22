feeefc25eea013cec4bf5e15bd7c9008bc35fb15
Claudia Simone de Souza <claudia.simmone@gmail.com>
2024-05-22 16:49:50 +0100

    240522

--- RSGymPT/Program.cs
+++ RSGymPT/Program.cs
@@ -13,9 +13,22 @@ namespace RSGymPT
         {
             RSGymUtility.SetUnicodeConsole();
 
-            Dictionary<User, User> usersDict = new Dictionary<User, User>();
+            List<User> users = new List<User>();
 
-            User user01 = new User("Claudia Simone de Souza", DateTime(1992, 12, 18), "clasi", "12345678");
+            
+            User user01 = new User("Claudia Simone de Souza", new DateTime(1992, 12, 18), "clasi", "12345678");
+            users.Add(user01);
+
+            User user02 = new User();
+            user02.Name = "Paula de Fátima Vallim Magalhães";
+            user02.Birth = new DateTime(1984, 12, 08);
+            user02.UserName = "paufa";
+            user02.Password = "87654321";
+
+            users.Add(user02);
+
+            User.ListUser(users);
+
             RSGymUtility.TerminateConsole();
         }
     }
